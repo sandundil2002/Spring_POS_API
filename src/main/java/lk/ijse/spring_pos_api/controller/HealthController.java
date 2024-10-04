1b77d761c29a13dfd04f4414f4486f669d59a605
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-10-04 12:52:03 +0530

    logback implemented.

--- src/main/java/lk/ijse/spring_pos_api/controller/HealthController.java
+++ src/main/java/lk/ijse/spring_pos_api/controller/HealthController.java
@@ -1,5 +1,7 @@
 package lk.ijse.spring_pos_api.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/health")
 public class HealthController {
 
+    static Logger logger = LoggerFactory.getLogger(HealthController.class);
+
     @GetMapping
     public String healthCheck() {
         System.out.println("Spring POS API is running");
+        logger.info("Spring POS API is running");
         return "Spring POS API is running";
     }
 
